3a7a08a8ee967ddde72dfe02a2d1526973f7a7ad
Adam Herro <adam.herro@comcast.net>
2021-10-01 16:40:50 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+letter_to_points = {key:value for key, value in zip(letters, points)}
+letter_to_points[' '] = 0
+
+def score_word(word):
+  point_total = 0
+  for letter in word:
+    point_total += letter_to_points[letter]
+  return point_total
+
+brownie_points = score_word('BROWNIE')
+
+
+player_to_words = {'player1': ['BLUE', 'TENNIS', 'EXIT'], 'wordNerd': ['EARTH', 'EYES', 'MACHINE'], 'Lexi Con': ['ERASER', 'BELLY', 'HUSKY'], 'Prof Reader': ['ZAP', 'COMA', 'PERIOD']}
+players_to_points = {}
+
+def update_point_totals():
+    for player, words in player_to_words.items():
+        player_points = 0
+        for word in words:
+            player_points += score_word(word)
+        players_to_points[player] = player_points
+    return players_to_points
+
+def play_word(player, word):
+    for key, value in player_to_words.items():
+        if key == player:
+            value.append(word)
+            
+            update_point_totals()
+    return value
+
+new_play = play_word('player1', 'YESTERDAY')            
+new_play2 = play_word('Prof Reader', 'CONGENIALITY')
+
+print(player_to_words)
+print(players_to_points)
+print(1)
